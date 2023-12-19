6cf2f9e19e69f5aae0f846670b132f921db4a113
Artem Moiseev <moiseev@physics.msu.ru>
2023-12-19 23:59:59 +0300

    finished stroke neon text

---
+++ JackpotCasino/Extensions/OutlinedText.swift
@@ -0,0 +1,48 @@
+//
+//  OutlinedText.swift
+//
+
+
+import SwiftUI
+
+struct OutlinedText: View {
+    var text: String
+    var width: CGFloat
+    var color: Color
+    let mainColor: Color
+    let font: Font
+    
+    var body: some View {
+        
+        let diagonal: CGFloat = 1/sqrt(2) * width
+        
+        ZStack{
+            ZStack{
+                // bottom right
+                Text(text).offset(x:  diagonal, y:  diagonal)
+                // top left
+                Text(text).offset(x: -diagonal, y: -diagonal)
+                // bottom left
+                Text(text).offset(x: -diagonal, y:  diagonal)
+                // top right
+                Text(text).offset(x:  diagonal, y: -diagonal)
+                // left
+                Text(text).offset(x:  -width, y: 0)
+                // right
+                Text(text).offset(x:  width, y: 0)
+                // top
+                Text(text).offset(x:  0, y: -width)
+                // bottom
+                Text(text).offset(x:  0, y: width)
+            }
+            .foregroundColor(color)
+            .font(font)
+            
+            Text(text)
+                .foregroundColor(mainColor)
+                .font(font)
+            
+        }
+        .padding()
+    }
+}
---
+++ JackpotCasino/Extensions/View+Border.swift
@@ -0,0 +1,47 @@
+//
+//  View+Border.swift
+//  JackpotCasino
+//
+//  Created by Artem on 19.12.2023.
+//
+
+import SwiftUI
+
+extension View {
+    @ViewBuilder
+    func viewBorder(color: Color = .black, radius: CGFloat = 0.4, outline: Bool = false) -> some View {
+        if outline {
+            self
+            
+                .shadow(color: color, radius: radius, x: 0, y: 1)
+                .shadow(color: color, radius: radius, x: 0, y: -1)
+                .shadow(color: color, radius: radius, x: 1, y: 0)
+                .shadow(color: color, radius: radius, x: -1, y: 0)
+                .invertedMask(
+                    self
+                )
+        } else {
+            self
+                .shadow(color: color, radius: radius, x: 0, y: 1)
+                .shadow(color: color, radius: radius, x: 0, y: -1)
+                .shadow(color: color, radius: radius, x: 1, y: 0)
+                .shadow(color: color, radius: radius, x: -1, y: 0)
+        }
+        
+    }
+}
+
+extension View {
+    func invertedMask<Content : View>(_ content: Content) -> some View {
+        self
+            .mask(
+                ZStack {
+                    self
+                        .brightness(1)
+                    content
+                        .brightness(-1)
+                }.compositingGroup()
+                    .luminanceToAlpha()
+            )
+    }
+}

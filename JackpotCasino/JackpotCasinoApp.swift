088605991291a72604077872b0d27fd002beb656
Artem Moiseev <moiseev@physics.msu.ru>
2023-12-19 22:24:31 +0300

    Initial Commit

---
+++ JackpotCasino/JackpotCasinoApp.swift
@@ -0,0 +1,17 @@
+//
+//  JackpotCasinoApp.swift
+//  JackpotCasino
+//
+//  Created by Artem on 19.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct JackpotCasinoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

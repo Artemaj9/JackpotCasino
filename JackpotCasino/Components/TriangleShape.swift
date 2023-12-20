c045203a26f5832880eb6189187e6fc94ddc5167
Artem Moiseev <moiseev@physics.msu.ru>
2023-12-20 23:57:38 +0300

    wheel almost done

---
+++ JackpotCasino/Components/TriangleShape.swift
@@ -0,0 +1,23 @@
+//
+//  TriangleShape.swift
+//
+
+import Foundation
+import SwiftUI
+
+struct Triangle: Shape {
+    
+    func path(in rect: CGRect) -> Path {
+        let center = CGPoint(x: rect.midX, y: rect.maxY)
+        let leftCorner = CGPoint(x: rect.minX, y: rect.minY)
+        let rightCorner = CGPoint(x: rect.maxX, y: rect.minY)
+        
+        var path = Path()
+        path.move(to: center)
+        path.addLine(to: leftCorner)
+        path.addLine(to: rightCorner)
+        path.closeSubpath()
+        
+        return path
+    }
+}
--- JackpotCasino/Screens/WelcomeView.swift
+++ JackpotCasino/Screens/WelcomeView.swift
@@ -14,52 +14,75 @@ struct WelcomeView: View {
             BackgroundView()
             VStack {
                 StrokedNeonText(text: "WELCOME!", color: Color("purpleText"), shadowColor: Color("purpleText"), size: 77)
-                    .padding(.top, 32)
-                
+                    .padding(.top)
+               
                 ZStack {
-                    ForEach(0..<count, id: \.self) { index in
-                        
-                        let angle1 = Angle(degrees: Double(index * 360) / Double(count))
-                        let angle2 = Angle(degrees: Double((index + 1) * 360) / Double(count))
-                        let angle3 = Angle(degrees: (angle1.degrees + angle2.degrees) / 2)
-                        Pie(startAngle: angle1, endAngle: angle2)
-                            .stroke(lineWidth: 4)
-                            .fill(.white)
-                            .shadow(color: Color("wheelShadow"), radius: 2)
-                            .shadow(color: Color("wheelShadow"), radius: 2)
-                        
-                       
-                    }
-                    .overlay {
-                        Circle()
-                            .stroke(lineWidth: 6)
-                            .shadow(color: Color("wheelShadow").opacity(0.2), radius: 4)
-                           //.glow(color: Color("wheelShadow"), radius: 1)
-                    }
-                    
-                    
-                    Circle()
-                        .shadow(color: Color("wheelShadow"), radius: 1)
-                        .frame(width: 48, height: 48)
-                    
-                    ForEach(0..<count, id: \.self) { index in
-                        HStack {
-                            Text(String(100*index))
-                                .font(Font.custom("RobotoCondensed-Bold", size:  24))
-                              
-                
-                            Image("coin")
-                                .resizable()
-                                .frame(width: 24, height: 24)
+                    Triangle()
+                        .rotationEffect(.degrees(180), anchor: .center)
+                        .shadow(color: Color("blueTextShadow"), radius: 4)
+                        .shadow(color: Color("blueTextShadow"), radius: 4)
+                    .frame(width: 60, height: 20)
+                    .offset(x: 30, y: -184)
+                    Triangle()
+                        .rotationEffect(.degrees(180), anchor: .center)
+                        .shadow(color: Color("blueTextShadow"), radius: 4)
+                        .shadow(color: Color("blueTextShadow"), radius: 4)
+                    .frame(width: 60, height: 20)
+                    .offset(x: -30, y: -184)
+                    ZStack {
+                        ForEach(0..<count, id: \.self) { index in
+                            
+                            let angle1 = Angle(degrees: Double(index * 360) / Double(count))
+                            let angle2 = Angle(degrees: Double((index + 1) * 360) / Double(count))
+                            let angle3 = Angle(degrees: (angle1.degrees + angle2.degrees) / 2)
+                            Pie(startAngle: angle1, endAngle: angle2)
+                                .fill(Color("wheelColor"))
+                            Pie(startAngle: angle1, endAngle: angle2)
+                                .stroke(lineWidth: 4)
+                                .fill(.white)
+                                .shadow(color: Color("wheelShadow"), radius: 2)
+                                .shadow(color: Color("wheelShadow"), radius: 2)
                         }
-                        .rotationEffect(Angle(radians:  Double(2 * index + 1) * .pi/Double(count)), anchor: .center)
+                        .overlay {
+                            Circle()
+                                .stroke(lineWidth: 6)
+                                .shadow(color: Color("wheelShadow").opacity(0.2), radius: 4)
+                        }
+                        
+                        Circle()
+                            .shadow(color: Color("wheelShadow"), radius: 1)
+                            .frame(width: 48, height: 48)
+                        
+                        ForEach(0..<count, id: \.self) { index in
+                            HStack {
+                                let text = index == 0 ?
+                                "250" : index == 9 ? "5000" : String(500*index)
+                                Text(text)
+                                    .font(Font.custom("Roboto-Bold", size:  24))
+                                    .shadow(color: Color("blueTextShadow"), radius: 2)
+                                    .shadow(color: Color("blueTextShadow"), radius: 1)
+                                    .overlay(   Text(text)
+                                        .font(Font.custom("Roboto-Bold", size:  24)))
+                                
+                                Image("coin")
+                                    .resizable()
+                                    .frame(width: 24, height: 24)
+                            }
+                            .rotationEffect(Angle(radians:  Double(2 * index + 1) * .pi/Double(count)), anchor: .center)
                             .offset(x: 130 * cos( Double(2 * index + 1) * .pi/Double(count)), y: 130 * sin(Double(2 * index + 1) * .pi/Double(count)))
+                        }
                     }
+                    .padding(.horizontal)
+                    // .compositingGroup()
+                    .rotationEffect(.radians(-.pi/2))
+                    .rotationEffect(.degrees(spin), anchor: .center)
+                   
+                    Triangle()
+                    .shadow(color: Color("blueTextShadow"), radius: 4)
+                    .shadow(color: Color("blueTextShadow"), radius: 4)
+                        .frame(width: 60, height: 50)
+                        .offset(y: -170)
                 }
-                .padding(.horizontal)
-               // .compositingGroup()
-                .rotationEffect(.radians(-.pi/2))
-                .rotationEffect(.degrees(spin), anchor: .center)
                 
                 Button {
                     withAnimation(.spring(response: 4, dampingFraction: 0.7)) {

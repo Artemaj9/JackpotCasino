1d5a947f65c997bd3bef8bbbcefe3421a000d44e
Artem Moiseev <moiseev@physics.msu.ru>
2023-12-22 03:05:02 +0300

    finished rewards screens

--- JackpotCasino/Components/RewardCell.swift
+++ JackpotCasino/Components/RewardCell.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct RewardCell: View {
     
     let index: Int
+    @State var isLocked: Bool = true
     
     var body: some View {
         ZStack {
@@ -24,6 +25,23 @@ struct RewardCell: View {
                     Image("reward\(index)")
                         .resizable()
                         .scaledToFit()
+                        .padding()
+                        .blur(radius: isLocked ? 5 : 0)
+                        .overlay {
+                            if isLocked {
+                                ZStack {
+                                    Image("cloud")
+                                        .resizable()
+                                        .scaledToFit()
+                                        .scaleEffect(0.7)
+                                        .shadow(color: Color("r\(index)Bg"), radius: 4)
+                                        .shadow(color: Color("r\(index)Bg"), radius: 4)
+                                    Text(index == 1 ? "1" : "\((index-1)*5)")
+                                        .font(Font.custom("RobotoCondensed-Bold", size: 30))
+                                        .foregroundColor(Color("cloudNum"))
+                                }
+                            }
+                        }
                 }
         }
         .padding(2)
---
+++ JackpotCasino/Screens/Info/InfoWelcome.swift
@@ -0,0 +1,20 @@
+//
+//  InfoWelcome.swift
+//  JackpotCasino
+//
+//  Created by Artem on 22.12.2023.
+//
+
+import SwiftUI
+
+struct InfoWelcome: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct InfoWelcome_Previews: PreviewProvider {
+    static var previews: some View {
+        InfoWelcome()
+    }
+}

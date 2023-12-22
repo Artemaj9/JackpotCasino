b51d810514d55669bb1815dc6606e2a010f14eb4
Artem Moiseev <moiseev@physics.msu.ru>
2023-12-22 20:54:30 +0300

    added header

--- JackpotCasino/Screens/MenuView.swift
+++ JackpotCasino/Screens/MenuView.swift
@@ -7,9 +7,47 @@
 
 import SwiftUI
 
+private let gradient  = LinearGradient(colors: [Color("questGradLight"), Color("questGradBright")], startPoint: .bottomLeading, endPoint: .topTrailing)
+
 struct MenuView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            BackgroundView()
+            HStack(spacing: 20) {
+                RoundedRectangle(cornerRadius: 8)
+                    .stroke(lineWidth: 4)
+                    .innerShadow(using:    RoundedRectangle(cornerRadius: 8), angle: .radians(0), color: .black.opacity(0.25), width: 1, blur: 1)
+                    .shadow(color: .black.opacity(0.25), radius: 1)
+                    .shadow(color: Color("menuRectShadow"), radius: 4)
+                    .shadow(color: Color("menuRectShadow"), radius: 2)
+                 
+                    
+                    .frame(height: 50)
+                    .overlay {
+                        HStack {
+                        Text("5000")
+                                .font(Font.custom("RobotoCondensed-Bold",size: 27))
+                        Image("coin")
+                            .resizable()
+                            .scaledToFit()
+                                }
+                    }
+                 
+                RoundedRectangle(cornerRadius: 8)
+                    .fill(gradient)
+                    .shadow(color: Color("questGradBright"), radius: 8)
+    
+                    .frame(width: 50,height: 50)
+                    .overlay(
+                    Image("quest")
+                        .resizable()
+                        .scaledToFit()
+                        .padding(10))
+                    
+            }
+            .padding(20)
+        }
+        .preferredColorScheme(.dark)
     }
 }
 
